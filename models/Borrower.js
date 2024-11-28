5987b42562fc78cf6c40505acced4d2cce13185b
Liquid Monks <info@liquidmonks.com>
2024-11-28 16:49:28 -0600

    Add mongoose schema for Borrower model in Borrower.js.

---
+++ models/Borrower.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const BorrowerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    bookId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Book',
+    },
+    borrowDate: {
+        type: Date,
+        default: Date.now,
+    },
+    returnDate: Date,
+});
+
+module.exports = mongoose.model('Borrower', BorrowerSchema);
