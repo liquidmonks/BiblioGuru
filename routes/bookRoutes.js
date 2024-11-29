4ff0fa365778c4cd3cf4be2a972e9fa06e66f9ea
Liquid Monks <info@liquidmonks.com>
2024-11-29 15:12:25 -0600

    Update routes in bookRoutes.js to require authentication middleware for all CRUD operations.

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
+const protect = require('../middleware/authMiddleware');
 
 // CRUD routes for books
 router.get('/', bookController.getAllBooks);
-router.post('/', bookController.addBook);
-router.get('/:id', bookController.getBookById);
-router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.post('/', protect, bookController.addBook);
+router.get('/:id', protect, bookController.getBookById);
+router.put('/:id', protect, bookController.updateBook);
+router.delete('/:id', protect, bookController.deleteBook);
 
 module.exports = router;
