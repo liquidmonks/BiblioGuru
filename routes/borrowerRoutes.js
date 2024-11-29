75cb6deedcb5ec922c1f201d69ce7dee2083210c
Liquid Monks <info@liquidmonks.com>
2024-11-29 15:13:18 -0600

    Update routes to include authentication middleware for protection.

--- routes/borrowerRoutes.js
+++ routes/borrowerRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const borrowerController = require('../controllers/borrowerController');
+const protect = require('../middleware/authMiddleware');
 
 // CRUD routes for borrowers
-router.get('/', borrowerController.getAllBorrowers);
-router.post('/', borrowerController.addBorrower);
-router.get('/:id', borrowerController.getBorrowerById);
-router.put('/:id', borrowerController.updateBorrower);
-router.delete('/:id', borrowerController.deleteBorrower);
+router.get('/', protect, borrowerController.getAllBorrowers);
+router.post('/', protect, borrowerController.addBorrower);
+router.get('/:id', protect, borrowerController.getBorrowerById);
+router.put('/:id', protect, borrowerController.updateBorrower);
+router.delete('/:id', protect, borrowerController.deleteBorrower);
 
 module.exports = router;
