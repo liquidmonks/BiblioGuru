392a3b73421c0afe0a660c8c7775016515a9211d
Liquid Monks <info@liquidmonks.com>
2024-12-02 16:20:16 -0600

    Refactor routing configuration and add Borrower Login page.

--- biblioguru-client/src/AppRouter.jsx
+++ biblioguru-client/src/AppRouter.jsx
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
 import BorrowerPage from './pages/BorrowerPage';
 import RegisterPage from './pages/RegisterPage';
+import BorrowerLoginPage from './pages/BorrowerLoginPage';
 
 function AppRouter() {
     return (
@@ -12,6 +13,7 @@ function AppRouter() {
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/register" element={<RegisterPage/>}/>
+                <Route path="/borrower-login" element={<BorrowerLoginPage/>}/>
             </Routes>
         </Router>
     );
